e330c7a6694aded8efa8a8841b3430c2313ab4a0
Dantain1 <57744176+Dantain1@users.noreply.github.com>
2024-06-03 17:23:36 +0100

    Update provision.sh
    rollback

--- provision.sh
+++ provision.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Function to display ASCII art
 print_ascii_art() {
-    echo -e "\033[0;32m"
+    echo -e "\033[0;31m"
     cat << "EOF"
- /$$   /$$ /$$    /$$$$$$$$ /$$$$$$ /$$      /$$  /$$$$$$ 
+/$$   /$$ /$$    /$$$$$$$$ /$$$$$$ /$$      /$$  /$$$$$$ 
 | $$  | $$| $$   |__  $$__/|_  $$_/| $$$    /$$$ /$$__  $$
 | $$  | $$| $$      | $$     | $$  | $$$$  /$$$$| $$  \ $$
 | $$  | $$| $$      | $$     | $$  | $$ $$/$$ $$| $$$$$$$$
@@ -16,7 +15,6 @@ EOF
     echo -e "\033[0m"
 }
 
-# Function to display a progress bar for a given command
 show_progress() {
     local -r msg="$1"
     local -r pid="$2"
@@ -31,37 +29,21 @@ show_progress() {
     printf "\r\033[1;33m$msg:\033[0m \033[1;34m[%-${width}s] \033[0;32mDONE!\033[0m\n" $(printf "#%.0s" $(seq 1 $width))
 }
 
-# Function to handle errors and exit
 error_exit() {
     echo "Error during $1. Exiting."
     echo "$2"
     exit 1
 }
 
-# Function to clean up temporary files
 cleanup() {
     rm -f /tmp/Nessus-10.7.3-raspberrypios_armhf.deb
 }
 
-# Function to get the IP address of the machine
 get_ip_address() {
     hostname -I | awk '{print $1}'
 }
 
-# Function to link Nessus to Tenable.io using the provided linking key
-link_nessus() {
-    local linking_key="$1"
-    /opt/nessus/sbin/nessuscli managed link --key=$linking_key
-    if [[ $? -ne 0 ]]; then
-        error_exit "linking Nessus to Tenable.io" "Failed to link Nessus"
-    fi
-}
-
-# Main script execution
 print_ascii_art
-
-# Prompt the user for the linking key
-read -p "Please enter your Tenable.io linking key: " LINKING_KEY
 
 # Update package lists
 update_output=$(sudo apt-get update -y 2>&1)
@@ -100,14 +82,10 @@ sudo systemctl enable nessusd >/dev/null 2>&1 &
 show_progress "Enabling Nessus service" $!
 wait $! || error_exit "enabling Nessus service"
 
-# Link Nessus to Tenable.io
-link_nessus $LINKING_KEY
-
 IP_ADDRESS=$(get_ip_address)
 PORT=8834
 
 cleanup
 
-# Display completion message and configuration URL
 echo -e "\033[0;32mINSTALLATION COMPLETE!\033[0m"
 echo "Please go here to continue configuration: http://$IP_ADDRESS:$PORT"
